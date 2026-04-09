96baedbdee56d7611aecc5c9fb67eff02bb307c3
rajmohan2004 <163750483+rajmohan2004@users.noreply.github.com>
2026-04-09 19:26:49 +0530

    Delete src directory

--- src/test/java/dataTabless/ninjasingledata.java
+++
@@ -1,9 +0,0 @@
-package dataTabless;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="./src/test/java/dataTabless/ninzasin.feature",glue="dataTabless",plugin= {"pretty","html:target/sin-report.html"},monochrome=true)
-public class ninjasingledata extends AbstractTestNGCucumberTests {
-
-}
